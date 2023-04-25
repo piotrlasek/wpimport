704054178a1846228f6c7af997b3a374ad196a65
piotr lasek <piotr.lasek@gmail.com>
2023-04-25 18:26:23 +0200

    adding spring boot app

---
+++ src/main/java/com/piotrlasek/wpimport/WpimportApplication.java
@@ -0,0 +1,13 @@
+package com.piotrlasek.wpimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpimportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpimportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piotrlasek/wpimport/WpimportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrlasek.wpimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpimportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
